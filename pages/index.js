9b103f4b74d7ce3ac63aaf03e3c71de41121b183
Joaquin <sol19008@byui.edu>
2021-04-18 20:36:42 -0300

    fixing styles on index page

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,25 @@ import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="page">
+    <div >
       <Head>
-        <title>Create Next App</title>
+        <title>Cafecito</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
-      <div className="" style={{ maxWidth: "64rem", margin: "auto" }}>
+      <div className="site">
         <Navbar />
         <Cta />
         <HowItWorks />
         <Comments />
         <Footer />
       </div>
-      <style jsx>{`
-      /* .page {
-        background: rgb(255,242,57);
-        background: linear-gradient(90deg, rgba(253, 230, 138) 0%,  #fbbf24 100%);     
-      }  */        
+      <style>{`
+      .site {
+        max-width: 64rem;
+        margin: auto;
+      }
       `}</style>
     </div>
   )
